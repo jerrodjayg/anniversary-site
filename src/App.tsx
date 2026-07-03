c10553e998fd6eb89e0ded3ed6bddba1508b211b
Jerrod Jaygbay <jerrodjaygbay123@gmail.com>
2026-07-02 23:44:38 -0400

    Remove music and shrink layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,9 @@ import Sky from "./components/Sky";
 import PhotoStack from "./components/PhotoStack";
 import FlowerMessage from "./components/FlowerMessage";
 import MermaidSequinBoard from "./components/MermaidSequinBoard";
-import AutoMusic from "./components/AutoMusic";
 import "./App.css";
 
 type Tab = "months" | "flower" | "board";
-
-const MUSIC_SRC =
-  "/Chik%C3%A9%20%26%20Simi%20%20Running%20(To%20You)%20%5BOfficial%20Audio%5D.mp3";
 
 export default function App() {
   const [tab, setTab] = useState<Tab>("months");
@@ -21,7 +17,6 @@ export default function App() {
   return (
     <>
       <Sky />
-      {tab === "months" && <AutoMusic src={MUSIC_SRC} volume={0.55} />}
       <div id="app">
         <header>
           <h1>Happy Anniversary ♡</h1>
